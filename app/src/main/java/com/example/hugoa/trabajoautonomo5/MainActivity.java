46214108c0291445b6324c4debaddf91680fb7da
Guido <guirespi@espol.edu.ec>
2019-07-20 18:12:59 -0500

    Implementado mapa de google e interfaz de actiidad principal

--- app/src/main/java/com/example/hugoa/trabajoautonomo5/MainActivity.java
+++ app/src/main/java/com/example/hugoa/trabajoautonomo5/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.hugoa.trabajoautonomo5;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Prueba Master
+    }
+
+    public void toCalendar(View view) {
+    }
+
+    public void toVideo(View view) {
+    }
+
+    public void cerrar(View view) {
+        finish();
+    }
+
+    public void toMap(View view) {
+        Intent i = new Intent(this,MapsActivity.class);
+        startActivity(i);
     }
 }
