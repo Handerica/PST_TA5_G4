880e627f04e7407fdcaa63fe9210d2fc9b0b220f
dlguerre <dlguerre@espol.edu.ec>
2019-07-21 05:52:00 -0500

    Creacion de rama Darly Guerrero e implementacion de calendario

---
+++ app/src/main/java/com/example/hugoa/trabajoautonomo5/Calendario.java
@@ -0,0 +1,33 @@
+package com.example.hugoa.trabajoautonomo5;
+
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+public class Calendario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendario);
+
+        CalendarView calendarView = findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+
+                String date = i + "/" + i1 + "/" + i2;
+
+                Intent intent = new Intent(Calendario.this, MainActivity.class);
+                intent.putExtra("date", date);
+
+                startActivity(intent);
+
+            }
+        });
+    }
+}
