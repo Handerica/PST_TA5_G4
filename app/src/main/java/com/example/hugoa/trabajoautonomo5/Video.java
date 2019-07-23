cce46fb44d74f0d9e9cc15af1917e563c33db9e3
Hugo <hugoandericau@bitbucket.org>
2019-07-23 15:22:41 -0500

    Actualizacion de las imagenes

--- app/src/main/java/com/example/hugoa/trabajoautonomo5/Video.java
+++ app/src/main/java/com/example/hugoa/trabajoautonomo5/Video.java
@@ -17,7 +17,7 @@ public class Video extends AppCompatActivity {
         setContentView(R.layout.activity_video);
 
         VideoView video_View = (VideoView) findViewById(R.id.video_View);
-        String video_Url = "rtsp://r6---sn-q4fl6n7y.googlevideo.com/Cj0LENy73wIaNAne9qSXOd-2pxMYDSANFC1rajZdMOCoAUIASARgvOvWu5fa8epbigELejZ4aWo5UlEyM2MM/2ECF48580AEF4C9213BD138E8609888E811FA4BA.298C2FCB635ECF0A240E5B71C4998452112C7B21/yt8/1/video.3gp";
+        String video_Url = "rtsp://r2---sn-q4fl6n7e.googlevideo.com/Cj0LENy73wIaNAmFN2T21wvqGxMYDSANFC2MajddMOCoAUIASARgvOvWu5fa8epbigELejZ4aWo5UlEyM2MM/448F6765A08AC4119B5DCBAB15BD281019945C90.D5C813BB43F7A266A9BCE0600F5FE482341A2009/yt8/1/video.3gp";
         Uri uri = Uri.parse(video_Url);
         video_View.setMediaController(new MediaController(this));
         video_View.setVideoURI(uri);
