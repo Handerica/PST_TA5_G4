12c31fd67c891e3abf3b8b732505b0581d55867f
Hugo <hugoandericau@bitbucket.org>
2019-07-23 19:14:04 -0500

    Actualizacion y correccion de Video de Youtube

---
+++ app/src/main/java/com/example/hugoa/trabajoautonomo5/YouTubeConfig.java
@@ -0,0 +1,13 @@
+package com.example.hugoa.trabajoautonomo5;
+
+public class YouTubeConfig {
+    public YouTubeConfig(){
+
+    }
+
+    private static final String API_KEY = "AIzaSyAIfS6Ed9_c0eULWqhtEIvMM-EyT_vmLp0";
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
