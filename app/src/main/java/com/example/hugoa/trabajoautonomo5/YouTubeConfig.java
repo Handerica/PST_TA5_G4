80fffbf9660b02b14d0ed99d8a6c0dc2f3676ae4
CisneFeijoo1998 <mdfeijoo@espol.edu.ec>
2019-07-23 21:20:15 -0500

    Comentarios de codig realizado en las actividades Main Activity y Video

--- app/src/main/java/com/example/hugoa/trabajoautonomo5/YouTubeConfig.java
+++ app/src/main/java/com/example/hugoa/trabajoautonomo5/YouTubeConfig.java
@@ -4,9 +4,10 @@ public class YouTubeConfig {
     public YouTubeConfig(){
 
     }
-
+    /*Se define la variable que contiene la clave del API */
     private static final String API_KEY = "AIzaSyAIfS6Ed9_c0eULWqhtEIvMM-EyT_vmLp0";
 
+    /*Se define el metodo que retorna la clave API, para que pueda ser utilizada en otras actividades*/
     public static String getApiKey() {
         return API_KEY;
     }
